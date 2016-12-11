90eb18815d79bcbd517198c8846da8ebd6137f8e
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2016-12-11 21:46:32 +0100

    Create LogMeIn.js

---
+++ LogMeIn.js
@@ -0,0 +1,10 @@
+document.getElementById("firstName").value = "Foo";
+document.getElementById("lastName").value = "Bar";
+document.getElementById("houseNameOrNumber").value = "Hauptstraße 88";
+document.getElementById("postCode").value = "26590 Berlin";
+document.getElementById("email").value = "" + Date.now() + "@tuta.io";
+document.getElementById("password").value = "admin1234";
+document.getElementById("confirmPassword").value = "admin1234";
+document.getElementById("question-pet").checked = true;
+document.getElementById("answer").value = "Marcel D'Avis";
+document.getElementById("terms").checked = true;
